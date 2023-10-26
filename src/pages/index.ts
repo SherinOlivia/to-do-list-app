318f59581985bde54acd4c7f9bedc4cb195d07c5
Sherin Olivia <SOChronicle@gmail.com>
2023-10-26 20:48:55 +0700

    feat: updated export for login, register, and page

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Register } from './Register'
+export { default as Login } from './Login'
+export { default as Profile } from './Profile'
+// export { default as CategoryList } from './CategoryList'
+// export { default as CreateNew } from './CreateNew'
+// export { default as UpdateCategory } from './UpdateCategory'
